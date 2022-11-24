1516b50a628e982cade60fae3ee9bdef35e2562d
MarquesRhaissa <rhaissa.marques@gmail.com>
2022-11-24 16:42:27 -0300

    feature - adding listItems and fixing app css

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,50 @@
-import styles from './App.css';
+import styles from "./App.module.css";
 import "./global.css";
 
 import { Header } from "./Components/Header";
-import { TaskBar } from './Components/TaskBar';
-import { TaskList } from './Components/TaskList';
+import { TaskBar } from "./Components/TaskBar";
+import { TaskList } from "./Components/TaskList";
+import { ListItems, ListItemsProps } from "./Components/ListItems";
+
+const items: ListItemsProps[] = [
+  {
+    id: "1",
+    content: "primeiro item da lista",
+    isDone: false,
+  },
+  {
+    id: "2",
+    content: "segundo item da lista",
+    isDone: true,
+  },
+  {
+    id: "3",
+    content: "terceiro item da lista",
+    isDone: false,
+  },
+];
 
 function App() {
-
   return (
     <>
-    <Header />
-    <TaskBar />
-    <TaskList />
+      <Header />
+      <div className={styles.wrapper}>
+        <main>
+          <TaskBar />
+          <TaskList />
+          {items.map((item) => {
+            return (
+              <ListItems
+                key={item.id}
+                content={item.content}
+                isDone={item.isDone}
+              />
+            );
+          })}
+        </main>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
